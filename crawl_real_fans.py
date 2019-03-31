499b185220157cde5e3ac8b6f620621a359e8af2
xiaoq <xiaoq@indiana.edu>
2019-03-30 20:44:18 -0400

    return the first 500 followers

--- crawl_real_fans.py
+++ crawl_real_fans.py
@@ -1,10 +1,16 @@
 """
-input: target_user_list
-output: followers of user in target_user_list
+Grab the followers info by the target user
+
+input: target user
+output: followers info
 """
 import sys
-from time import time, sleep
+from time import sleep
 
+import pandas as pd
+from instapy import InstaPy
+from instapy import set_workspace
+from instapy import smart_run
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
@@ -20,7 +26,8 @@ from util.util import web_adress_navigator
 
 chrome_options = Options()
 chromeOptions = webdriver.ChromeOptions()
-prefs = {'profile.managed_default_content_settings.images': 2, 'disk-cache-size': 4096}
+prefs = {'profile.managed_default_content_settings.images': 2,
+         'disk-cache-size': 4096}
 chromeOptions.add_experimental_option("prefs", prefs)
 chrome_options.add_argument('--dns-prefetch-disable')
 chrome_options.add_argument('--no-sandbox')
@@ -42,31 +49,34 @@ def get_user_info(browser, username):
     alias = ""
     container = browser.find_element_by_class_name('v9tJq')
     isprivate = False
+
     try:
         if container.find_element_by_class_name('Nd_Rl'):
             isprivate = True
-    except:
+    except BaseException:
         isprivate = False
 
     try:
         alias = container.find_element_by_class_name('-vDIg').find_element_by_tag_name('h1').text
-    except:
+    except BaseException:
         InstaLogger.logger().info("alias is empty")
 
     try:
-        bio = container.find_element_by_class_name('-vDIg').find_element_by_tag_name('span').text
-    except:
+        bio = container.find_element_by_class_name(
+            '-vDIg').find_element_by_tag_name('span').text
+    except BaseException:
         InstaLogger.logger().info("Bio is empty")
 
     try:
         bio_url = container.find_element_by_class_name('yLUwa').text
-    except:
+    except BaseException:
         InstaLogger.logger().info("Bio Url is empty")
 
     try:
         img_container = browser.find_element_by_class_name('RR-M-')
-        prof_img = img_container.find_element_by_tag_name('img').get_attribute('src')
-    except:
+        prof_img = img_container.find_element_by_tag_name(
+            'img').get_attribute('src')
+    except BaseException:
         InstaLogger.logger().info("image is empty")
 
     try:
@@ -74,32 +84,19 @@ def get_user_info(browser, username):
 
         try:
             num_of_posts = extract_exact_info(infos[0])
-        except:
+        except BaseException:
             InstaLogger.logger().error("Number of Posts empty")
 
         try:
             following = {'count': extract_exact_info(infos[2])}
-        except:
+        except BaseException:
             InstaLogger.logger().error("Following is empty")
 
         try:
             followers = {'count': extract_exact_info(infos[1])}
-
-            try:
-                if isprivate is True:
-                    InstaLogger.logger().info("Cannot get Follower List - private account")
-                else:
-                    followers['list'] = extract_followers(browser, username)
-                    # print(followers['list'])
-            except Exception as exception:
-                # Output unexpected Exceptions.
-                print("Unexpected error:", sys.exc_info()[0])
-                print(exception)
-
-                InstaLogger.logger().error("Cannot get Follower List")
-        except:
+        except BaseException:
             InstaLogger.logger().error("Follower is empty")
-    except:
+    except BaseException:
         InstaLogger.logger().error("Infos (Following, Abo, Posts) is empty")
 
     information = {
@@ -122,101 +119,7 @@ def get_user_info(browser, username):
     InstaLogger.logger().info("Following: " + str(information['following']['count']))
     InstaLogger.logger().info("isPrivate: " + str(information['isprivate']))
 
-    for id, fan in enumerate(information['followers']['list']):
-        print(id, fan)
     return information
-
-
-def extract_followers(browser, username):
-    InstaLogger.logger().info('Extracting follower from ' + username)
-    try:
-        user_link = "https://www.instagram.com/{}".format(username)
-        web_adress_navigator(browser, user_link)
-    except PageNotFound404 as e:
-        raise NoInstaProfilePageFound(e)
-    sleep(5)
-
-    followers = []
-
-    # find number of followers
-    elem = browser.find_element_by_xpath(
-        "//span[@id='react-root']//header[@class='vtbgv ']//ul[@class='k9GMp ']/child::li[2]/a/span")
-    elem.click()
-    sleep(15)
-
-    # remove suggestion list and load 24 list elements after this
-    browser.execute_script("document.getElementsByClassName('isgrP')[0].scrollTo(0,500)")
-    sleep(10)
-
-    elems = browser.find_elements_by_xpath("//body//div[@class='PZuss']//a[@class='FPmhX notranslate _0imsa ']")
-    num = len(elems)
-    print('debug info:', num)
-    for i in range(num):
-        val = elems[i].get_attribute('innerHTML')
-        print('debug info -- first user:', i, val)
-        followers.append(val)
-
-    try:
-        for i in range(num):
-            browser.execute_script("document.getElementsByClassName('PZuss')[0].children[0].remove()")
-    except:
-        pass
-
-    isDone = False
-    while True:
-        try:
-            start = time()
-            browser.execute_script(
-                "document.getElementsByClassName('isgrP')[0].scrollTo(0,document.getElementsByClassName('isgrP')[0].scrollHeight)")
-
-            while True:
-                try:
-                    loop = int(
-                        browser.execute_script("return document.getElementsByClassName('PZuss')[0].children.length"))
-                    if loop == num * 2:
-                        break
-                except (KeyboardInterrupt, SystemExit):
-                    raise
-                except:
-                    continue
-                if time() - start > 10:
-                    isDone = True
-                    break
-
-            if isDone:
-                break
-
-            elems = browser.find_elements_by_xpath("//body//div[@class='PZuss']//a[@class='FPmhX notranslate _0imsa ']")
-            num = len(elems)
-            print('debug info:', num)
-            list_segment = ""
-            for i in range(12):
-                val = elems[i].get_attribute('innerHTML')
-                list_segment += (val + '\n')
-                print('debug info -- middle user:', i, val)
-                followers.append(val)
-
-            for i in range(12):
-                browser.execute_script("document.getElementsByClassName('PZuss')[0].children[0].remove()")
-
-            print(time() - start)
-
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except:
-            continue
-
-    list_segment = ""
-    elems = browser.find_elements_by_xpath("//body//div[@class='PZuss']//a[@class='FPmhX notranslate _0imsa ']")
-    num = len(elems)
-    print('debug info:', num)
-    for i in range(len(elems)):
-        val = elems[i].get_attribute('innerHTML')
-        list_segment += (val + '\n')
-        print('debug info -- last user:', i, val)
-        followers.append(val)
-
-    return followers
 
 
 def extract_information(browser, username):
@@ -234,7 +137,31 @@ def extract_information(browser, username):
     return userinfo
 
 
-def run(usernames=['nightmello']):
+# https://github.com/timgrossmann/InstaPy#grab-followers-of-a-user
+def grab_followers(target_user='nightmello'):
+    # set workspace folder at desired location (default is at your home folder)
+    set_workspace(path=None)
+
+    # get an InstaPy session!
+    session = InstaPy(username=Settings.login_username,
+                      password=Settings.login_password,
+                      headless_browser=True)
+
+    with smart_run(session):
+        selected_followers = session.grab_followers(
+            username=target_user,
+            amount="full",
+            live_match=True,
+            store_locally=True)
+
+        return selected_followers
+
+
+def find_real_fans(target_user='nightmello'):
+    followers_list = grab_followers(target_user)
+    sleep(30)
+
+    fan_list = {}
     try:
         browser = init_chromedriver(chrome_options, capabilities)
     except Exception as exc:
@@ -242,17 +169,22 @@ def run(usernames=['nightmello']):
         sys.exit()
 
     try:
-        for username in usernames:
-            print('Extracting information from ' + username)
+        login(
+            browser,
+            Settings.login_username,
+            Settings.login_password)
+
+        for user in followers_list:
+            print('Extracting information from ' + user)
             try:
-                login(browser, Settings.login_username, Settings.login_password)
-                information = extract_information(browser, username)
-            except:
-                print("Error with user " + username)
+                information = extract_information(browser, user)
+                fan_list[user] = information
+            except BaseException:
+                print("Error with user " + user)
                 sys.exit(1)
 
-            Datasaver.save_profile_json(username, information)
-            print("\nFinished.\n")
+            Datasaver.save_profile_json(user, information)
+        print("\nFinished.\n")
 
     except KeyboardInterrupt:
         print('Aborted...')
@@ -261,5 +193,20 @@ def run(usernames=['nightmello']):
         browser.delete_all_cookies()
         browser.close()
 
+    df = pd.DataFrame(columns=['alias', 'private', 'num_posts', 'num_followers', 'num_following'])
+    for id, element in enumerate(fan_list):
+        alias = element
+        is_private = fan_list[element]['isprivate']
+        num_posts = fan_list[element]['num_of_posts']
+        num_followers = fan_list[element]['followers']['count']
+        num_following = fan_list[element]['following']['count']
+        info = [alias, is_private, num_posts, num_followers, num_following]
+        tmp = pd.DataFrame([info], columns=['alias', 'private', 'num_posts', 'num_followers', 'num_following'])
+        df = df.append(tmp, ignore_index=True)
+        print(id, info)
 
-run()
+    df.to_csv('real_fans_of_{}.csv'.format(target_user), sep='\t', encoding='utf-8')
+    return df
+
+
+find_real_fans('nightmello')
